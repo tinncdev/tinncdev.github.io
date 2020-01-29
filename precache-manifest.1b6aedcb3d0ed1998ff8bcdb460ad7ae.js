b26f5941fafb0ebebb1aba23abf3c0e8c9428258
Tin Nguyen <tinnguyen263@outlook.com>
2020-01-29 10:29:05 +0000

    Updates

---
+++ precache-manifest.1b6aedcb3d0ed1998ff8bcdb460ad7ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9eee948210f08d77d3f32e070bcd1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d659ba94395406283b08",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "26bd97ec904f991ed91e",
+    "url": "/static/js/2.a7c37d20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a7c37d20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d659ba94395406283b08",
+    "url": "/static/js/main.0a2e4ffd.chunk.js"
+  },
+  {
+    "revision": "749ef65e3c1719b95837",
+    "url": "/static/js/runtime-main.b2f09445.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
